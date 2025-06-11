54ece82e1eb44527f1172d45a4289fc17b0b88cd
connect-spesifickly <mozar.sosial@gmail.com>
2025-06-11 14:57:51 +0700

    erase button

--- src/components/about/page.tsx
+++ src/components/about/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import * as React from "react";
-import { Button } from "../ui/button";
 import Image from "next/image";
 import { useState } from "react";
 import dynamic from "next/dynamic";
